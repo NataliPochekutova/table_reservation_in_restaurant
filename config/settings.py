d36dcb8bda7f1db844e12bf3c75a96d341a4def8
Natalia Pochekutova <natalipochekutova51@gmail.com>
2025-10-20 23:31:09 +0800

    flake8

--- config/settings.py
+++ config/settings.py
@@ -3,10 +3,12 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 
 SECRET_KEY = os.getenv("SECRET_KEY")
